6a2b729a3e76f7c4d4727bf175de605c8d14699f
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2015-07-28 18:05:17 -0700

    working F estimation

--- BasicFunctions.py
+++ BasicFunctions.py
@@ -2,6 +2,7 @@
 Provide some basic helper functions.
 """
 
+import cv2
 import numpy as np
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
@@ -13,14 +14,65 @@ def f2K(f):
 				   [0, f, 0],
 				   [0, 0, 1]], dtype=np.float)
 
-def drawMatches(img1, kp1, img2, kp2, matches):
-	""" Visualize keypoint matches. """
+def E2pose(E):
+    """ Convert essential matrix to pose. From H&Z p.258"""
 
-	"""
-	WORK ON THIS!
-	"""
+    W = np.matrix([[0, -1, 0],
+                   [1, 0, 0],
+                   [0, 0, 1]], dtype=np.float)
+    Z = np.matrix([[0, 1, 0],
+                   [-1, 0, 0],
+                   [0, 0, 0]], dtype=np.float)
 
-	pass
+    U, D, V = np.linalg.svd(E)
+
+    # skew-symmetric translation matrix 
+    #S = U * Z * U.T
+
+    # two possibilities for R, t
+    R1 = U * W * V.T
+    R2 = U * W.T * V.T
+
+    t1 = U[:, 2]
+    t2 = -U[:, 2]
+
+    # ensure positive determinants
+    if np.linalg.det(R1) < 0
+        R1 = -R1
+
+    if np.linalg.det(R2) < 0
+        R2 = -R2
+
+
+
+def drawMatches(img1, kp1, img2, kp2, matches, matchesMask):
+    """ Visualize keypoint matches. """
+
+    # get dimensions
+    h1, w1 = img1.shape[:2]
+    h2, w2 = img2.shape[:2]
+
+    # create display
+    view = np.zeros((max(h1, h2), w1 + w2, 3), np.uint8)
+
+    view[:h1, :w1, :] = img1
+    view[:h2, w2:, :] = img2
+
+    color = (0, 255, 0)
+
+    if matchesMask is None:
+        return
+
+    for idx, m in enumerate(matches):
+        if matchesMask[idx] == 1:
+            cv2.line(view, 
+                     (int(kp1[m.queryIdx].pt[0]), int(kp1[m.queryIdx].pt[1])), 
+                     (int(kp2[m.trainIdx].pt[0] + w1), int(kp2[m.trainIdx].pt[1])), 
+                     color)
+
+    cv2.imshow("Keypoint correspondences", view)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
 def imread(imfile):
     """ Read image from file and normalize. """
--- simpleSFM.py
+++ simpleSFM.py
@@ -2,18 +2,21 @@
 SimpleSFM main file. Based on SFMedu v2 by Jianxiong Xiao.
 """
 
-import os
+import os, sys
 import BasicFunctions as bf
 import numpy as np
 import matplotlib.image 
 import cv2
 import matplotlib.pyplot as plt
 
-visualize = True
+# parameters
+visualize = False
+RATIO = 0.2
+MIN_MATCHES = 20
 
 # set up
-IMPATH = "../Images/"
-files = os.listdir(IMPATH)
+IMPATH = "Images/"
+files = [f for f in os.listdir(IMPATH) if not f.startswith(".")]
 
 frames = {}
 frames["images"] = np.array(files)
@@ -31,22 +34,42 @@ matcher = cv2.BFMatcher(normType=cv2.NORM_HAMMING)
 # for adjacent frames, detect ORB keypoints and extimate F
 for i in range(frames["num_images"] - 1):
 
-	# read in images
-	img1 = cv2.cvtColor(cv2.imread(IMPATH + frames["images"][i]), cv2.COLOR_BGR2GRAY)
-	img2 = cv2.cvtColor(cv2.imread(IMPATH + frames["images"][i+1]), cv2.COLOR_BGR2GRAY)
+    # read in images
+    img1 = cv2.imread(IMPATH + frames["images"][i])
+    img2 = cv2.imread(IMPATH + frames["images"][i + 1])
 
-	# get keypoints
-	kp1, des1 = orb.detectAndCompute(img1, None)
-	kp2, des2 = orb.detectAndCompute(img2, None)
+    img1 = np.flipud(np.fliplr(cv2.resize(img1, dsize=(0, 0), fx=RATIO, fy=RATIO)))
+    img2 = np.flipud(np.fliplr(cv2.resize(img2, dsize=(0, 0), fx=RATIO, fy=RATIO)))
 
-	# do keypoint matching
-	matches = matcher.knnMatch(des1, des2, k=2)
+    gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-	# store all the good matches as per Lowe's ratio test.
-	good_matches = []
-	for m, n in matches:
-	    if m.distance < 0.7*n.distance:
-        	good_matches.append([m])
+    # get keypoints
+    kp1, des1 = orb.detectAndCompute(gray1, None)
+    kp2, des2 = orb.detectAndCompute(gray2, None)
 
-	if visualize:
-		bf.drawMatches(img1, kp1, img2, kp2, good_matches)
+    # do keypoint matching
+    matches = matcher.knnMatch(des1, des2, k=2)
+
+    # store all the good matches as per Lowe's ratio test.
+    good_matches = []
+    for m, n in matches:
+        if m.distance < 0.7 * n.distance:
+            good_matches.append(m)
+
+    # estimate F if sufficient good matches
+    if len(good_matches) >= MIN_MATCHES:
+        pts1 = np.array([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1,1,2)
+        pts2 = np.array([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1,1,2)
+
+        F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.RANSAC, 5.0)
+        matchesMask = mask.ravel().tolist()
+    else:
+        print "Did not find enough good matches (%d/%d)" % (len(good),MIN_MATCHES)
+        sys.exit(0)
+    
+    if visualize:
+        bf.drawMatches(img1, kp1, img2, kp2, good_matches, matchesMask)
+
+    # get E from F and convert to poses
+    
